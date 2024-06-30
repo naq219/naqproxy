e7e1b13df7941ea667f86cd8737db8678696b4a0
Your Name <you@example.com>
2024-06-30 16:06:52 +0700

    init

---
+++ proxy_setting.js
@@ -0,0 +1,18 @@
+exports.proxySetting2 = function() {
+    var proxySetting1 = {
+        'pac_script_url' : {'http': '', 'https': '', 'file' : ''},
+        'pac_type'   : 'file://',
+        'http_host'  : '139.162.17.99',
+        'http_port'  : '14754',
+        'https_host' : '',
+        'https_port' : '',
+        'socks_host' : '',
+        'socks_port' : '',
+        'socks_type' : 'socks5',
+        'bypasslist' : '<local>,192.168.0.0/16,172.16.0.0/12,169.254.0.0/16,10.0.0.0/8',
+        'proxy_rule' : 'singleProxy',
+        'internal'   : '',
+        'auth'       : {'enable': 'true', 'user': '5zfZ', 'pass': 'ZXX8'}
+    }
+    return proxySetting1
+};  
